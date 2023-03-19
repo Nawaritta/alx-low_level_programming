ee0bc88bfe4190d0d8d09ebb0d2ab0a4d1d56fd2
Nawaritta <nor.benhaddou@gmail.com>
2023-03-19 01:14:49 +0000

    task 0 mod

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
-     		printf("is positive\n");
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
 	else if (n < 0)
+	{
 		printf("is negative\n");
+	}
 	else
 		printf("is zero\n");
 	return (0);
