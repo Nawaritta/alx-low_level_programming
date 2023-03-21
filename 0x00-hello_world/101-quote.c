a4b44af61e0dd5fd7d2c5597b23e3981463e4388
Nawaritta <nor.benhaddou@gmail.com>
2023-03-21 03:04:53 +0000

    betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: always -1 (Failure)
+ * Return: always 1 (Failure)
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	Printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("\n");
 	return (1);
 }
