7b7412d7aba89c04e4b5faada1f9611f382bad9e
Nawaritta <nor.benhaddou@gmail.com>
2023-03-21 02:22:04 +0000

    using char table

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
-return (0);
+	int = i;
+	char quote[70] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	i = 0;
+	do {
+		putchar(quote[i]);
+		i++;
+	} while (i < strlen(quote));
+
+	return (0);
 }
