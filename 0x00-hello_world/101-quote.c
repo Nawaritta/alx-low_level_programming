5ffc04774af126b105da29f08f9211e2dbc2ea2b
Nawaritta <nor.benhaddou@gmail.com>
2023-03-21 03:00:38 +0000

    task 8 trace write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
 }
