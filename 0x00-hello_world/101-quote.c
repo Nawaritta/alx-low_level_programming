9b62c6975ac37a81456b2446c79db1f216ecc976
Nawaritta <nor.benhaddou@gmail.com>
2023-03-21 01:54:41 +0000

    implicit function declaration

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-	return (0);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
+return (0);
 }
