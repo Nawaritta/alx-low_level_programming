468cc2c67b470e139e8f05e558e7aefa186a079e
Nawaritta <nor.benhaddou@gmail.com>
2023-03-21 23:24:49 +0000

    putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+void print_alphabet_x10(void);
+{
+	char a;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (a = 'a'; a < = 'z' ; a++)
+		{
+		putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
