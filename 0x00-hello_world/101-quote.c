296f21a88651b4a6f5c56c7e53b125b3f95444db
Nawaritta <nor.benhaddou@gmail.com>
2023-03-21 02:32:55 +0000

    101 mod

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	int i;
-	char qt[70] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char qt[70] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	i = 0;
 	do {
 		putchar(qt[i]);
 		i++;
 	} while (i < strlen(qt));
-
+	putchar('\n');
 	return (0);
 }
