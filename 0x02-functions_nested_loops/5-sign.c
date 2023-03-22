637babe2fc9ce45c1da313661c04019fbd0d9c8b
Nawaritta <nor.benhaddou@gmail.com>
2023-03-22 23:42:21 +0000

    sign fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,12 @@ int print_sign(int n)
 	{
 		_putchar('-');
 		x = -1;
-		else if(n > 0)
-		{
-			_putchar('+');
-			x = 1;
-			else
-				_putchar('0');
-			x = 0;
-		}
-	}
+	}else if(n > 0)
+	{
+		_putchar('+');
+		x = 1;
+	}else
+		_putchar('0');
+		x = 0;
 	return (x);
 }
