2dba4dc86df4e724aeb348feda92912d7729bd27
Nawaritta <nor.benhaddou@gmail.com>
2023-03-24 20:44:46 +0000

    readme updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,9 +10,10 @@ void print_number(int n)
 	if (n > 9)
 	{
 		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 	} else
 	{
-		if (n > 0)
+		if (n > 0 && )
 		{
 			_putchar((n % 10) + '0');
 		} else
