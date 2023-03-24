cfeb59f846635f3ce64c69771da81e02ad35ae55
Nawaritta <nor.benhaddou@gmail.com>
2023-03-24 23:42:52 +0000

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always
+ */
+int main(void)
+{
+        int i = 3;
+	long n = 612852475143;
+
+	while (i != n)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+		} else
+		{
+			i++;
+		}
+	}
+	printf("%ld\n", n);
+	return (0);
+}
