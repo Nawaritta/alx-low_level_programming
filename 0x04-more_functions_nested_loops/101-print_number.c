fcd7a103e5448820719ec056253c8799c730140b
Nawaritta <nor.benhaddou@gmail.com>
2023-03-25 03:07:19 +0000

    You are not allowed to hard-code special values

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,36 +11,30 @@ void print_number(int n)
 	int j;
 	int d;
 
-	if (n == 0)
-	{
-		_putchar('0');
-	} else
-	{
-		d = 0;
-		i = n;
-		do {
-			i /= 10;
-			++d;
-		} while (i != 0);
+	d = 0;
+	i = n;
+	do {
+		i /= 10;
+		++d;
+	} while (i != 0);
 
-		if (n < 0)
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	} else
+		i = n;
+	while (d > 0)
+	{
+		j = d - 1;
+		a = 1;
+		while (j > 0)
 		{
-			_putchar('-');
-			i = -n;
-		} else
-			i = n;
-		while (d > 0)
-		{
-			j = d - 1;
-			a = 1;
-			while (j > 0)
-			{
-				a *= 10;
-				j--;
-			}
-			_putchar((i / a) + '0');
-			i = i - ((i / a) * a);
-			d--;
+			a *= 10;
+			j--;
 		}
+		_putchar((i / a) + '0');
+		i = i - ((i / a) * a);
+		d--;
 	}
 }
