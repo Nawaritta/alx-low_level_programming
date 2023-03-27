496230289dfa90337ef5e0d21269a46e22b25cf0
Nawaritta <nor.benhaddou@gmail.com>
2023-03-27 23:19:57 +0000

    task 5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,9 @@ void print_rev(char *s)
 		++len;
 	}
 
-	while (len >= 0)
+	while (len-1 >= 0)
 	{
-		_putchar(s[len]);
+		_putchar(s[len-1]);
 		len--;
 	}
 	_putchar('\n');
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string
+ *@s: string to revers
+ */
+void rev_string(char *s)
+{
+	int len;
+	char *stemp;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		++len;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		stemp[len - 1 - i] = s[i];
+	}
+	s = stemp;
+	_putchar('\n');
+}
