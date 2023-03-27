cde7b79d30864f771345186bafa349d099fd79ae
Nawaritta <nor.benhaddou@gmail.com>
2023-03-27 21:34:44 +0000

    task 2 betty style

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  *_puts - print string
- *
+ *@str: point to a string
  */
 void _puts(char *str)
 {
 	int i;
 
-	for(i = 0; i <= _strlen(str); i++)
+	for (i = 0; i <= _strlen(str); i++)
 	{
 		_putchar(str[i]);
 	}
