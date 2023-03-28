7add16c35a201410ac2f97cdf24834a23486d08f
Nawaritta <nor.benhaddou@gmail.com>
2023-03-28 00:52:45 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ void puts_half(char *str)
 		++len;
 	}
 
-	if (len % 2)
+	if (len % 2 == 0)
 		n = len / 2;
 	else
 		n = (len - 1) / 2;
 
-	for (i = n; i <= len; i++)
+	for (i = n; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
