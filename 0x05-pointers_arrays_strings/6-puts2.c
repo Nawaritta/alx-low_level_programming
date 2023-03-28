6aec7d7c3327474fa2610fbfef2c152669882efd
Nawaritta <nor.benhaddou@gmail.com>
2023-03-28 00:19:57 +0000

    task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,9 +5,15 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
+	len = 0;
+	while (str[len] != '\0')
+	{
+		++len;
+	}
 	i = 0;
-	while (str[i] != '\0')
+	while (i <= len)
 	{
 		_putchar(str[i]);
 		i += 2;
