1f3827ed4ff6863ffe2d312c8c9a34d2df2f763d
Nawaritta <nor.benhaddou@gmail.com>
2023-03-30 17:45:49 +0000

    strcmp funtion

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,10 @@ int _strcmp(char *s1, char *s2)
 	int k = 0;
 
 	while (s1[k] != '\0' && s2[k] != '\0')
+	{
 		k++;
-	if (s1[k] != '\0' && s2[k] == '\0')
-		ret = 15;
-	if (s1[k] == '\0' && s2[k] != '\0')
-		ret = -15;
+		ret = s1[k] - s2[k];
+	}
+
 	return (ret);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,19 +6,14 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	int j = 0;
 
 	while (str[i] != '\0')
 	{
-		if (j == 0)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
-		}
-		j = 1;
+		if (str[i] >= 'a' && str[i] <= 'z')
+	   		str[i] = str[i] - 32;
+		while (str[i] != ('\t' && '\n' && ' ' && '.' && ';' && ',' && '(' && ')' && '{' && '{' && '?' && '!' && '"'))
+			i++;
 
-		if (str[i] == ' ' || str[i] == '.' || str[i] == '\0')
-			j = 0;
 		i++;
 	}
 	return (str);
