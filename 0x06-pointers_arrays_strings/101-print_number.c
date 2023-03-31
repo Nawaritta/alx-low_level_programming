4a9598e4bcbdedb86540c42e13c0a790f522f477
Nawaritta <nor.benhaddou@gmail.com>
2023-03-31 00:38:38 +0000

    same task?

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *print_number - prints number
+ *@n: integer to print
+ */
+void print_number(int n)
+{
+	int a;
+	int i;
+	int j;
+	int d;
+
+	d = 0;
+	i = n;
+	do {
+		i /= 10;
+		++d;
+	} while (i != 0);
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	} else
+		i = n;
+
+	while (d > 0)
+	{
+		j = d - 1;
+		a = 1;
+		while (j > 0)
+		{
+			a *= 10;
+			j--;
+		}
+		_putchar((i / a) + '0');
+		i = i - ((i / a) * a);
+		d--;
+	}
+}
