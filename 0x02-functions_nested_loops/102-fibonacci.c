688dca38242f8f6d8a329083dec686214272a23d
Nawaritta <nor.benhaddou@gmail.com>
2023-04-02 16:51:34 +0000

    ..

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *return: always zero
+ */
+int main(void)
+{
+	int fn_1 = 0;
+	int fn_2 = 1;
+	int fn = fn_1 + fn_2;
+	int i = 2;
+	int Fib;
+
+	while (i < 50)
+	{
+		fn = fn_2
+			fn+ = fn_1;
+	}
+	printf("%d\n", Fib);
+	return (0);
+}
