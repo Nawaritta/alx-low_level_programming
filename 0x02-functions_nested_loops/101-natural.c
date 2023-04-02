7c37a6a24d69ad4c2c4314a929ea11bb4187507d
Nawaritta <nor.benhaddou@gmail.com>
2023-04-02 03:31:33 +0000

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - enrty point
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+	int sum = 23;
+
+	for (i = 10; i < 1024; i++)
+	{
+		if (i % 3 || i % 5)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
