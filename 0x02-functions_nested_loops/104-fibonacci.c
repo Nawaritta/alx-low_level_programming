a40619c9bc55f82e59a3a2a47e42d1491a156617
Nawaritta <nor.benhaddou@gmail.com>
2023-04-03 01:17:57 +0000

    fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,9 +9,9 @@ int main(void)
 	float fn_2 = 1;
 	float fn_1 = 2;
 	float fn = fn_1 + fn_2;
-       	int j = 3;
+	int j = 3;
 
-	printf("%.0f, %.0f, ", fn_2,fn_1);
+	printf("%.0f, %.0f, ", fn_2, fn_1);
 
 	while (j <= 98)
 	{
