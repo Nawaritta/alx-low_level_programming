854ee71e61893ca2210b7979dadbcc0f5e3dd9f4
Nawaritta <nor.benhaddou@gmail.com>
2023-04-03 01:55:25 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-	Printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		putchar(c[i]);
+		i++;
+	}
 	return (1);
 }
