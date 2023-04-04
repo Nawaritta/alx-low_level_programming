6bafbe68e3af0141e27bffccd79ca85fee52e748
Nawaritta <nor.benhaddou@gmail.com>
2023-04-04 02:59:20 +0000

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,8 +7,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int diag1 = ;
-	int diag2 = ;
-	int *b = ;
+	int diag1 = 0;
+	int diag2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		diag1 += a[(i * size) + i];
+		diag2 += a[(size - 1) + ((size - 1) * i)];
+	}
 	printf("%d, %d\n", diag1, diag2);
 }
