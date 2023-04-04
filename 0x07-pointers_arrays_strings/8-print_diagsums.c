4ea2f8c4d8b74f6aae00e651667392482d9efd45
Nawaritta <nor.benhaddou@gmail.com>
2023-04-04 01:28:38 +0000

    chessboard fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,18 +7,8 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int diag1 = *a;
-	int diag2 = *(a + size);
-	int *b = a;
-
-	while(*a != '\0')
-	{
-		a += size + 1;
-		printf("%ls, %p",a,&a);
-		diag1 += *a;
-
-		diag2 += *(b + size - 1);
-		b += size - 1;
-	}
+	int diag1 = ;
+	int diag2 = ;
+	int *b = ;
 	printf("%d, %d\n", diag1, diag2);
 }
