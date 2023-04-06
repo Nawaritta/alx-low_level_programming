90a143f391143c616965efaca6edaf0b856c439a
Nawaritta <nor.benhaddou@gmail.com>
2023-04-06 22:59:16 +0000

    sqrt_NO_recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+/**
+ *is_palindrome - checks if a number is palindrome
+ *@s: to check
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+
+	if (s*)
+	{
+		if (s[i] == s[len-i-1])
+			_is_palindrome(++s);
+		return (1);
+	}
+	return (1);
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,21 @@
 #include "main.h"
+#include "4-pow_recursion.c"
 /**
- * _sqrt_recursion - returns square root
- * @n: integer to return
- * Return: returns square root
+ *_sqrt_recursion - gives the natural square root of a number
+ *@n: number
+ *Return: square root or -1
  */
 int _sqrt_recursion(int n)
 {
-    
+	int sq = 0;
+
+	while(sq <= n)
+	{
+		if (_pow_recursion(sq, 2) != n)
+			sq++;
+		else
+			return (sq);
+	}
+
+	return (-1);
 }
