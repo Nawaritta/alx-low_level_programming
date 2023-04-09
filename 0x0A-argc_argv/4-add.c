797b8cd0afc72834cdd153c31b812e9833a3c3b0
Nawaritta <nor.benhaddou@gmail.com>
2023-04-09 17:57:43 +0000

    add positifs numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - adds two numbers
+ *@argc: The number of command line arguments
+ *@argv: An array containing the program command line arguments
+ *Return: sum of two integers, 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int k;
+	int sum = 0;
+
+	if (argc < 2)
+		printf("0\n");
+
+	else
+	{
+		for (; i < argc; i++)
+		{
+
+			k = atoi(argv[i]);
+			if (k != 0 && k >= 0)
+				sum += k;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (sum);
+	}
+	return (0);
+}
