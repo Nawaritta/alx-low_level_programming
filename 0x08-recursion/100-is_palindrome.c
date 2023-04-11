6fe744d89701239011b667be09344610fea763ce
Nawaritta <nor.benhaddou@gmail.com>
2023-04-11 03:34:10 +0000

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,11 +7,10 @@ int hel_per(char *s, char *a)
 {
 	if (*s == *a)
 	{
-		if (&s == &a)
-		{
+		if (s >= a)
 			return (1);
-		}
-		return (hel_per(++s,--a));
+
+		return (hel_per(++s, --a));
 	}
 	return (0);
 }
@@ -24,5 +23,5 @@ int is_palindrome(char *s)
 	if (*s == '\0')
 		return (1);
 
-	return (hel_per(s,a));
+	return (hel_per(s, a));
 }
