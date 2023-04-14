0c4689a4a6e7ae8807cbbcd978e0002d7170aa29
Nawaritta <nor.benhaddou@gmail.com>
2023-04-14 00:39:06 +0000

    t2 allocate the exacte size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	Ar = malloc((nmemb * size) + 1);
+	Ar = malloc(nmemb * size);
 
 	if (Ar == NULL)
 		return (NULL);
