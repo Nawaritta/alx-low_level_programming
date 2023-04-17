b523b5838130173ed2cb698a9ff3d4cb962d2879
Nawaritta <nor.benhaddou@gmail.com>
2023-04-17 18:15:42 +0000

    initialization

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef dog_h
 #define dog_h
+
 /**
  * struct dog - dog structure
  * @name: dog's name
@@ -16,5 +17,9 @@ struct dog
 };
 
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
