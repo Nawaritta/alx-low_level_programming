cdd3d686682eb4062c1a5652a2fd2adfd9bffed6
Nawaritta <nor.benhaddou@gmail.com>
2023-04-17 04:18:12 +0000

    realloc two checks failed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,8 +34,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		new_ptr = ptr;
 
 		memset((char *)ptr + new_size, 0, old_size - new_size);
-
-		free(ptr);
 	}
 
 	return (new_ptr);
