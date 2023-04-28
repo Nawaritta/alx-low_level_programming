46fd8e7f896efcefcc304981f809d4155fe5e009
Nawaritta <Nor.benhaddou@gmail.com>
2023-04-28 02:15:34 +0000

    betty task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,7 @@
 /**
  * add_node_end - adds a node at the end a list
  * @head: pointer to the pointer to the head node
+ * @str: string of the added node
  * Return: the address of the new element or null if failed
  */
 list_t *add_node_end(list_t **head, const char *str)
