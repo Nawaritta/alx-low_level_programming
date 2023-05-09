4891e3a0d83757cf8a1b7a35dd530deb4ecdd892
Nawaritta <Nor.benhaddou@gmail.com>
2023-05-09 19:17:25 +0000

    if file_to already exists, truncate it

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,19 +7,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, readc, writtenc, permissions = 664;
+	int fd_from, fd_to, readc, writtenc;
 	char buffer[BUFF_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: file_from file_to\n");
 		exit(97); }
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(argv[1], 0664);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
