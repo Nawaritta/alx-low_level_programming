2c154914944d0c41f2e456d019c1d58a97f4e29e
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-20 01:27:16 +0000

    fixing wget lowercase

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-Wget -P $PWD https://github.com/Nawaritta/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libjackpot.so 
+wget -P $PWD https://github.com/Nawaritta/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libjackpot.so 
 export LD_PRELOAD=/$PWD/libjackpot.so
