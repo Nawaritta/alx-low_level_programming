141943ee32c0c3135b1a6e83ff702c8dce7a0796
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-20 01:37:11 +0000

    fixing the library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P $PWD https://github.com/Nawaritta/alx-low_level_programming/tree/master/0x18-dynamic_libraries/lib_jackpot.so 
+Wget -P $PWD https://github.com/Nawaritta/alx-low_level_programming/tree/master/0x18-dynamic_libraries/lib_jackpot.so 
 export LD_PRELOAD=/$PWD/lib_jackpot.so
