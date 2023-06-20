0e7089ea35b5cdffee4c19a09b05c0369d083327
Nawaritta <Nor.benhaddou@gmail.com>
2023-06-20 01:57:27 +0000

    using tmp

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-Wget -P $PWD https://github.com/Nawaritta/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib_jackpot.so
-export LD_PRELOAD=/$PWD/lib_jackpot.so
+Wget -P /tmp/ https://github.com/Nawaritta/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib_jackpot.so
+export LD_PRELOAD=/tmp/lib_jackpot.so
