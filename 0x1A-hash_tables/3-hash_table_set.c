bcd8dd2d8590c676344f75fbd79a8c0ad00ccc2e
Nawaritta <Nor.benhaddou@gmail.com>
2023-07-07 23:52:36 +0000

    task 6 - happy valgrind

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,31 +9,45 @@
  * Return: 0 if succeeded or otherwise - 1 if failed.
  */
 
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_hn;
 	unsigned long int i;
+	char *value_cpy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
+	value_cpy = strdup(value);
+	if (value_cpy == NULL)
+		return (0);
+
 	i = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[i] != NULL && strcmp(ht->array[i]->key, key) == 0)
+	for (i = i; ht->array[i]; i++)
 	{
-		ht->array[i]->value = strdup(value);
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cpy;
+			return (1);
+		}
 	}
-
 
 	new_hn = malloc(sizeof(hash_node_t));
 	if (new_hn == NULL)
+	{
+		free(value_cpy);
 		return (0);
-
+	}
 	new_hn->key = strdup(key);
-	new_hn->value = strdup(value);
+	if (new_hn->key == NULL)
+	{
+		free(new_hn);
+		return (0);
+	}
+	new_hn->value = value_cpy;
 	new_hn->next = ht->array[i];
-
 	ht->array[i] = new_hn;
 
 	return (1);
