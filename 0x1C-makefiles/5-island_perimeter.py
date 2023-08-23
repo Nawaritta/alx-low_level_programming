64407c6e7bb85a8b006b03cb2a623cd4eede2511
Nawaritta <Nor.benhaddou@gmail.com>
2023-08-23 18:10:27 +0000

    edge cases fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,17 +7,25 @@ def island_perimeter(grid):
     Returns the perimeter of the island described in grid
     """
     perim = 0
-    if grid and len(grid) > 0:
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
-                        perim += 1
-                    if j - 1 >= 0 and grid[i][j - 1] == 0:
-                        perim += 1
-                    if i + 1 < len(grid) and grid[i + 1][j] == 0:
-                        perim += 1
-                    if i - 1 >= 0 and grid[i][j + 1] == 0:
-                        perim += 1
+    if not grid or len(grid) == 0:
+        return perim
+
+    width = len(grid)
+    height = len(grid[0])
+
+    if width > 100 or height > 100:
+        return
+
+    for i in range(width):
+        for j in range(height):
+            if grid[i][j] == 1:
+                if j + 1 >= height or (j + 1 < height and grid[i][j + 1] == 0):
+                    perim += 1
+                if j - 1 < 0 or (j - 1 >= 0 and grid[i][j - 1] == 0):
+                    perim += 1
+                if i + 1 >= width or (i + 1 < width and grid[i + 1][j] == 0):
+                    perim += 1
+                if i - 1 < 0 or (i - 1 >= 0 and grid[i][j + 1] == 0):
+                    perim += 1
 
     return perim
