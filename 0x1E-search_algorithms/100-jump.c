1294979245189c3cd0cc739ee6e833a5fc3a8626
Nawaritta <Nor.benhaddou@gmail.com>
2023-10-11 22:07:20 +0000

    should be ok

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,15 +21,27 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
 
-		if (!bounded && (array[idx + step] >= value || idx + step >= size))
+		if (!bounded)
+		{
+			if (idx + step < size)
+			{
+				if (array[idx + step] >= value)
+				{
+					bounded = 1;
+					size = idx + step + 1;
+				}
+			} else
+				bounded = 1;
+		}
+		if (bounded == 1)
 		{
 			printf("Value found between indexes [%ld]", idx);
 			printf(" and [%ld]\n", idx + step);
 			printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
-			bounded = 1;
 			step = 1;
-
+			bounded = 2;
 		}
+
 		if (array[idx] == value)
 			return (idx);
 		idx += step;
